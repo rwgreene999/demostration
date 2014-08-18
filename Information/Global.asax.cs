cff586d590daac0a6caa92967fbe1365b7468ac1
rwg <rwg14@morinmoss.com>
2014-08-18 11:09:33 -0600

    Added ServucePointManager to get google maps to work

--- Information/Global.asax.cs
+++ Information/Global.asax.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -22,6 +23,7 @@ namespace Information
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3;
             LoadQuoteFile();
         }
 
